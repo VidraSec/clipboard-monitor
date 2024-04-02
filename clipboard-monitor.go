5dbe152a514e806ebfd100f8f5a39df13b75311c
Martin Grottenthaler <9715799+mgrottenthaler@users.noreply.github.com>
2024-04-02 10:52:14 +0300

    initial code

---
+++ clipboard-monitor.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/atotto/clipboard"
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	// helper variable to prevent writing to file when the program has just written to it
+	programWroteToFile := false
+	// helper variable to prevent writing to clipboard when the program has just written to it
+	programWroteToClipboard := false
+
+	filePath := "clipboard.txt"
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalf("Error creating watcher: %s", err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write && !programWroteToFile {
+					fmt.Println("Detected file change, updating clipboard")
+					content, err := os.ReadFile(filePath)
+					if err != nil {
+						log.Printf("Error reading file: %s", err)
+						continue
+					}
+					if err := clipboard.WriteAll(string(content)); err != nil {
+						log.Printf("Error writing to clipboard: %s", err)
+					}
+					programWroteToClipboard = true
+				} else if programWroteToFile {
+					// if the program wrote to the file, reset the flag
+					programWroteToFile = false
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Printf("Watcher error: %s", err)
+			}
+		}
+	}()
+
+	// Ensure the file exists before starting to watch it
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Println("File does not exist, creating file.")
+		// Create the file with empty content
+		if err := os.WriteFile(filePath, []byte(""), 0644); err != nil {
+			log.Fatalf("Failed to create %s: %s", filePath, err)
+		}
+	}
+
+	err = watcher.Add(filePath)
+	if err != nil {
+		log.Fatalf("Error adding watcher to file: %s", err)
+	}
+
+	// Initial read and clipboard set
+	initialContent, err := os.ReadFile(filePath)
+	if err == nil {
+		clipboard.WriteAll(string(initialContent))
+	}
+
+	// Monitor clipboard changes and update the file
+	previousClipboardContent, _ := clipboard.ReadAll()
+	for {
+		time.Sleep(1 * time.Second)
+		currentClipboardContent, _ := clipboard.ReadAll()
+		if currentClipboardContent != previousClipboardContent && !programWroteToClipboard {
+			if err := os.WriteFile(filePath, []byte(currentClipboardContent), 0644); err != nil {
+				log.Printf("Error writing to file: %s", err)
+			}
+			previousClipboardContent = currentClipboardContent
+			programWroteToFile = true
+		} else if programWroteToClipboard {
+			// if the program wrote to the clipboard, reset the flag
+			programWroteToClipboard = false
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module vidrasec/clipboard-monitor
+
+go 1.18
+
+require (
+	github.com/atotto/clipboard v0.1.5-0.20210311235813-1438b322bde4
+	github.com/fsnotify/fsnotify v1.7.1-0.20231207092505-c94b93b06027
+)
+
+require golang.org/x/sys v0.4.0 // indirect
